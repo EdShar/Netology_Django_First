c65d6abd8d97969c5ece9d7583c45b2077641987
EdShar <dvjtarantul@gmail.com>
2024-02-01 20:35:34 +0300

    orm_migrations commit

--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -6,6 +6,10 @@ from .models import Student
 
 def students_list(request):
     template = 'school/students_list.html'
+
+    object_list = Student.objects.order_by('group')
+    context = {'object_list': object_list}
+
     context = {}
 
     # используйте этот параметр для упорядочивания результатов
