70c21c3188032b04dfa6cd9a59f55ab268afb7c1
EdShar <dvjtarantul@gmail.com>
2024-02-01 21:02:02 +0300

    m2m-relations commit

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,37 @@
 from django.contrib import admin
+from .models import Article, Tag, Scope
 
-from .models import Article
+from django.forms import BaseInlineFormSet
+from django.core.exceptions import ValidationError
 
+
+class RelationshipInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        for form in self.forms:
+            cleaned_data = form.cleaned_data
+            if 'is_main' in cleaned_data and cleaned_data['is_main']:
+                if self.instance.pk:
+                    raise ValidationError('Основной тег может быть указан только один раз')
+                else:
+                    if self.initial[0].get('is_main', False):
+                        raise ValidationError('Ошибка: Основной тег уже выбран')
+                    else:
+                        pass
+        return super().clean()
+
+
+class RelationshipInline(admin.TabularInline):
+    model = Tag
+    formset = RelationshipInlineFormset
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [RelationshipInline]
+
+@admin.site.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+@admin.site.register(Scope)
+class ScopeAdmin(admin.ModelAdmin):
     pass
